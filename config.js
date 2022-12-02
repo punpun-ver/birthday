abb9e9d8c7c21a54ecb23b427a04c1389ecadcca
punpun-ver <118261411+punpun-ver@users.noreply.github.com>
2022-12-02 07:27:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "Hôm nay sinh nhật bạn.",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "Mình chúc bạn",  // 同上...
-        "Sinh nhật vẻ",
-        "Cùng với người bạn yêu",
-        "Tình yêu của mình",
-        "Mình xin lỗi",
-        "Mình thích bạn",
-        "Vậy nên chúc bạn sinh nhật vui vẻ",
-        "Và ... hạnh phúc",
-        "5/12/2022",
-        "duongthuy",
+        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "心爱的小可爱",  // 同上...
+        "今天是你的生日",
+        "这是我们在一起的",
+        "第三个生日了哦",
+        "去年的生日",
+        "还记得在王婆串串吃的呢",
+        "今年要吃好的喽哦",
+        "要把我家可爱猪猪喂饱饱",
+        "然后抱走",
+        "YAMI~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "Nhấn",
-        play: "Nhấn",
-        bannar_coming: "Nhấn",
-        balloons_flying: "Tiếp tục nhấn",
-        cake_fadein: "Chào <3",
-        light_candle: "Bạn gái mình",
-        wish_message: "Bạn đẹp quá",
-        story: "Mình có điều muốn nói với bạn!",
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
     }
 };
